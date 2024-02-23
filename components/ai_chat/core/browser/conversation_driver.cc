a0844f5ad2ff60b8c392c02793d9e78f0ab5b357
Claudio DeSouza <cdesouza@brave.com>
2024-02-23 18:48:41 +0000

    Removing straggling `absl::optional` references
    This polyfill has been cleaned up from the codebase for a long time
    already. From M124 onwards these references are now failing to build.
    
    Chromium change:
    https://chromium.googlesource.com/chromium/src/+/902f8eb73215d22ba4342a6ca82031e508cfbda2
    
    commit 902f8eb73215d22ba4342a6ca82031e508cfbda2
    Author: Ayu Ishii <ayui@chromium.org>
    Date:   Wed Feb 21 09:25:18 2024 +0000
    
        CacheStorage: Remove deprecated CacheStorageHelper
    
        This change removes the deprecated CacheStorageHelper.
        CacheStorage usage and deletion is handled by the QuotaClient.
    
        Bug: 40826521

--- components/ai_chat/core/browser/conversation_driver.cc
+++ components/ai_chat/core/browser/conversation_driver.cc
@@ -995,7 +995,7 @@ void ConversationDriver::RateMessage(
           std::move(callback).Run(id);
           return;
         }
-        std::move(callback).Run(absl::nullopt);
+        std::move(callback).Run(std::nullopt);
       },
       std::move(callback));
 
@@ -1018,7 +1018,7 @@ void ConversationDriver::RateMessage(
     return;
   }
 
-  std::move(callback).Run(absl::nullopt);
+  std::move(callback).Run(std::nullopt);
 }
 
 void ConversationDriver::SendFeedback(
